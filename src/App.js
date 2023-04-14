94a2800102230c529cbae010fb3f94c1dc0d4c21
JohnMnyika <jmnyika514@gmail.com>
2023-04-14 18:29:29 +0300

    Added the geolocation clicks on a table

--- src/App.js
+++ src/App.js
@@ -1,25 +1,69 @@
-import React, { useState, useEffect } from "react";
+import React, { useState, useEffect } from 'react';
 
 function ClickCounter() {
-    const [count, setCount] = useState(0);
+    const [count, setCount] = useState(() => {
+        const savedCount = localStorage.getItem('count');
+        return savedCount !== null ? parseInt(savedCount, 10) : 0;
+    });
+    const [clicksByCountry, setClicksByCountry] = useState({});
+
+    function handleClick() {
+        const country = window.navigator.geolocation.getCurrentPosition((position) => {
+            const { latitude, longitude } = position.coords;
+            fetch(`https://nominatim.openstreetmap.org/reverse?format=jsonv2&lat=${latitude}&lon=${longitude}`)
+                .then((response) => response.json())
+                .then((data) => {
+                    const country = data.address.country;
+                    setClicksByCountry(prevClicksByCountry => ({
+                        ...prevClicksByCountry,
+                        [country]: (prevClicksByCountry[country] || 0) + 1,
+                    }));
+                })
+                .catch((error) => {
+                    console.error(error);
+                });
+        });
+        setCount(prevCount => {
+            const newCount = prevCount + 1;
+            localStorage.setItem('count', newCount);
+            return newCount;
+        });
+    }
 
     useEffect(() => {
-        const storedCount = localStorage.getItem("clickCount");
-        if (storedCount) {
-            setCount(parseInt(storedCount));
+        const savedClicksByCountry = localStorage.getItem('clicksByCountry');
+        if (savedClicksByCountry !== null) {
+            setClicksByCountry(JSON.parse(savedClicksByCountry));
         }
     }, []);
 
-    const handleClick = () => {
-        setCount(count + 1);
-        localStorage.setItem("clickCount", count + 1);
-    };
+    useEffect(() => {
+        localStorage.setItem('clicksByCountry', JSON.stringify(clicksByCountry));
+    }, [clicksByCountry]);
 
     return (
-        <div className="text-center">
+        <div className='text-center'>
             <h1>Light-hall Counter App</h1>
-            <p>Clicks: {count}</p>
-            <button className="btn btn-primary" onClick={handleClick}>Click here</button>
+            <p className='p'>Number of clicks: {count}</p>
+            <button className='btn btn-primary btn-lg' onClick={handleClick}>Click here</button>
+                <div className='table-responsive-md'>
+                    <table className='table table-striped'>
+                        <thead>
+                        <tr>
+                            <th>Country</th>
+                            <th>Clicks</th>
+                        </tr>
+                        </thead>
+                        <tbody>
+                        {Object.entries(clicksByCountry).map(([country, clicks]) => (
+                            <tr key={country}>
+                                <td>{country}</td>
+                                <td>{clicks}</td>
+                            </tr>
+                        ))}
+                        </tbody>
+                    </table>
+                </div>
         </div>
     );
 }
